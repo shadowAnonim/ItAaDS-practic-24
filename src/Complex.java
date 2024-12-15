9651e989abb6ad054dc581d29c1ac0d425a6b4fa
Аноним Евгений <shadow.anonim@yandex.ru>
2024-12-15 13:07:49 +0300

    Initial commit

---
+++ src/Complex.java
@@ -0,0 +1,9 @@
+public class Complex {
+    public int real;
+    public int imaginary;
+
+    public Complex(int real, int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+}
